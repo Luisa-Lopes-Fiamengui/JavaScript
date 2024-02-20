d5fe36d4be39ad49a382624f98ba289fdd91190b
Pikanavee <luisamengui07@gmail.com>
2024-02-20 14:55:46 -0300

    entrega de atividades

--- atividade02.js
+++ atividade02.js
@@ -9,6 +9,7 @@ var resultado = (x -= y);
 console.log("ele tem", resultado, "anos");
 
 console.log("------------------------");
+
 var comprimento = 12;
 var largura = 6;
 console.log(12 * 6);
@@ -16,6 +17,29 @@ var area = 72;
 console.log("a área do retangulo é de", area, "cm");
 
 console.log("------------------------");
+
 var dim = 5;
 var vol = dim ** 5;
 console.log("o valor da altura é", dim, "e o volume é de", vol);
+
+console.log("------------------------");
+
+var a = 2
+var b = 3
+var c = -1
+var delta = b**2-4*a*c
+console.log(delta)
+
+console.log("------------------------");
+
+var compra = 450
+var desconto = 3/100
+var valDesc = compra * desconto
+console.log("compra de r$"+compra+" - desconto r$"+valDesc+ " | total = r$")
+
+console.log("------------------------");
+
+var tempo =  3.5
+var velocidade = 95
+var dist = tempo*velocidade
+console.log("distancia percorrida é de "+dist+"km")
